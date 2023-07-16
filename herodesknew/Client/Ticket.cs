b3e63f8558b64fc542cdbc8c1b6d12de18948e0e
welrikysonfelix <welrikyson_felix@outlook.com>
2023-07-16 18:24:49 -0400

    CC

---
+++ herodesknew/Client/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace herodesknew.Client
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public required int IdDepartment { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string UserEmail { get; set; }
+        public required int SlaUsed { get; set; }
+        public bool IsClosed { get; set; }
+       
+    }
+}
+
--- herodesknew/Domain/Entities/Attachment.cs
+++ herodesknew/Domain/Entities/Attachment.cs
@@ -9,10 +9,8 @@ namespace herodesknew.Domain.Entities
     public class Attachment
     {
         public int Id { get; set; }
-        //TODO: Usar FIle {Name, Path, Content}
-        public string FileName { get; set; }
-        public string FilePath { get; set; }
-        public byte[] FileContent { get; set; }
+        public required string FileName { get; set; }
+        public required string FilePath { get; set; }
         public int TicketId { get; set; }
     }
 }
