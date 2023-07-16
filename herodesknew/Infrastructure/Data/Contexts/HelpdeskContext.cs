7e74528775a00bc6c7655062f2a071d494756882
Dev 01 <wfmcosta@simpress.com.br>
2023-07-16 16:04:33 -0300

    sync

---
+++ herodesknew/Infrastructure/Data/Contexts/HelpdeskContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace herodesknew.Infrastructure.Contexts
+{
+    public class HelpdeskContext
+    {
+        private readonly string HelpdeskConnectionString;
+
+        public HelpdeskContext(IConfiguration configuration)
+        {
+            HelpdeskConnectionString = configuration.GetConnectionString("HelpdeskSqlConnection")!;
+        }
+
+        public IDbConnection CreateDbConnection() => new SqlConnection(HelpdeskConnectionString);
+    }
+}
---
+++ herodesknew/Server/Controllers/TicketController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace herodesknew.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly ILogger<TicketController> _logger;
+
+        public TicketController(ILogger<TicketController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
