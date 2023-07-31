543cd3404511cff17aced742874a2ab74bf3cc00
Dev 01 <wfmcosta@simpress.com.br>
2023-07-31 10:41:16 -0300

    service installer

--- herodesknew/Server/Program.cs
+++ herodesknew/Server/Program.cs
@@ -1,74 +1,13 @@
-using herodesknew.Application.Attachments.Queries.GetAttachment;
-using herodesknew.Application.PullRequests.Commands.CreatePullRequest;
-using herodesknew.Application.PullRequests.Queries.GetPullRequests;
-using herodesknew.Application.SqlExecutionPlans.Commands.UseSqlExecutionPlan;
-using herodesknew.Application.SqlFiles.Commands.CreateSqlFile;
-using herodesknew.Application.SqlFiles.Commands.OpenSqlFile;
-using herodesknew.Application.Tickets.Queries.GetFilteredTickets;
-using herodesknew.Application.Tickets.Queries.GetTicket;
-using herodesknew.Domain.AppSettingEntities;
-using herodesknew.Domain.Repositories;
-using herodesknew.Infrastructure.Data.Contexts;
-using herodesknew.Infrastructure.Data.Repositories;
-using Scrutor;
+using herodesknew.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Install(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services
-    .Scan(
-        selector => selector
-            .FromAssemblies(
-                herodesknew.Infrastructure.AssemblyReference.Assembly,
-                 herodesknew.Local.Domain.AssemblyReference.Assembly)
-            .AddClasses(false)
-            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
-            .AsMatchingInterface()
-            .WithScopedLifetime());
-
-#region services install
-//TODO: Refactory service install 
-builder.Services.AddTransient<GetFilteredTicketsQueryHandler>();
-builder.Services.AddTransient<GetTicketQueryHandler>();
-builder.Services.AddTransient<GetAttachmentQueryHandler>();
-builder.Services.AddTransient<GetPullRequestsQueryHandler>();
-builder.Services.AddTransient<CreatePullRequestCommandHandler>();
-builder.Services.AddTransient<OpenSqlFileCommandHandler>();
-builder.Services.AddTransient<SqlExecutionPlanCommandHandler>();
-builder.Services.AddTransient<CreateSqlFileCommandHandler>();
-
-builder.Services.AddTransient<herodesknew.Local.Application.SqlFiles.Commands.OpenSqlFile.OpenSqlFileCommandHandler>();
-builder.Services.AddTransient<herodesknew.Local.Application.SqlFiles.Commands.CreateSqlFile.CreateSqlFileCommandHandler>();
-builder.Services.AddTransient<herodesknew.Local.Application.Tickets.Queries.GetTickets.GetTicketsQueryHandler>();
-builder.Services.AddTransient<herodesknew.Local.Application.SqlExecutionDocs.Commands.UseSqlExecutionDoc.UseSqlExecutionDocCommandHandler>();
-
-
-
-builder.Services.AddDbContext<HerodesknewDbContext>();
-builder.Services.AddTransient<HelpdeskContext>();
-
-var azureReposSettings = builder.Configuration.GetSection("AzureReposSettings").Get<AzureReposSettings>()!;
-builder.Services.AddSingleton(azureReposSettings);
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAllOrigins",
-        builder =>
-        {
-            builder.AllowAnyOrigin()
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
-});
-
-#endregion
-
 
 var app = builder.Build();
 
---
+++ herodesknew/Server/ServiceInstaller.cs
@@ -0,0 +1,61 @@
+﻿using herodesknew.Application.Attachments.Queries.GetAttachment;
+using herodesknew.Application.PullRequests.Commands.CreatePullRequest;
+using herodesknew.Application.PullRequests.Queries.GetPullRequests;
+using herodesknew.Application.SqlExecutionPlans.Commands.UseSqlExecutionPlan;
+using herodesknew.Application.SqlFiles.Commands.CreateSqlFile;
+using herodesknew.Application.SqlFiles.Commands.OpenSqlFile;
+using herodesknew.Application.Tickets.Queries.GetFilteredTickets;
+using herodesknew.Application.Tickets.Queries.GetTicket;
+using herodesknew.Domain.AppSettingEntities;
+using herodesknew.Infrastructure.Data.Contexts;
+using Scrutor;
+
+namespace herodesknew.Server;
+
+public static class ServiceInstaller
+{
+    public static void Install(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services
+            .Scan(
+                selector => selector
+                    .FromAssemblies(
+                        Infrastructure.AssemblyReference.Assembly,
+                        Local.Domain.AssemblyReference.Assembly)
+                    .AddClasses(false)
+                    .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                    .AsMatchingInterface()
+                    .WithScopedLifetime());
+
+        services.AddTransient<GetFilteredTicketsQueryHandler>();
+        services.AddTransient<GetTicketQueryHandler>();
+        services.AddTransient<GetAttachmentQueryHandler>();
+        services.AddTransient<GetPullRequestsQueryHandler>();
+        services.AddTransient<CreatePullRequestCommandHandler>();
+        services.AddTransient<OpenSqlFileCommandHandler>();
+        services.AddTransient<SqlExecutionPlanCommandHandler>();
+        services.AddTransient<CreateSqlFileCommandHandler>();
+
+        services.AddTransient<Local.Application.SqlFiles.Commands.OpenSqlFile.OpenSqlFileCommandHandler>();
+        services.AddTransient<Local.Application.SqlFiles.Commands.CreateSqlFile.CreateSqlFileCommandHandler>();
+        services.AddTransient<Local.Application.Tickets.Queries.GetTickets.GetTicketsQueryHandler>();
+        services.AddTransient<Local.Application.SqlExecutionDocs.Commands.UseSqlExecutionDoc.UseSqlExecutionDocCommandHandler>();
+
+        services.AddDbContext<HerodesknewDbContext>();
+        services.AddTransient<HelpdeskContext>();
+        
+        services.AddSingleton(configuration.GetSection("AzureReposSettings").Get<AzureReposSettings>()!);
+        
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAllOrigins",
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+        });            
+    }
+}
